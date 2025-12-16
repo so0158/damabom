0e3efd755865652df28ee26955be9dd2ecc94028
so0158 <sunshine.lee.sl@gmail.com>
2025-12-16 17:24:57 +0900

    쇼핑몰 test

---
+++ src/main/java/com/project/damabom/DamabomApplication.java
@@ -0,0 +1,13 @@
+package com.project.damabom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamabomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamabomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/damabom/DamabomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.damabom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamabomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
